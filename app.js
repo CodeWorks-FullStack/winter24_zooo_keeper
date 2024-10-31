9d1e76631b8480ff455132acf79e0e84995e8649
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-31 10:07:04 -0600

    feeding jeffrey

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Javascript is ready!');
+//#region state
+const animals = [
+  {
+    name: 'jeffrey',
+    emoji: '🐅',
+    mood: 100
+  },
+  {
+    name: 'polka-dot',
+    emoji: '🦓',
+    mood: 100
+  },
+]
+
+//#endregion
+
+//#region logic
+
+function feedJeffrey() {
+  // ✅ get jeffrey
+  const jeffrey = animals[0]
+  // make mood go up by 1
+  jeffrey.mood += 1
+  console.log('this is jeffrey', jeffrey)
+}
+
+
+
+//#endregion
+
+//#region graphics
+
+//#endregion
