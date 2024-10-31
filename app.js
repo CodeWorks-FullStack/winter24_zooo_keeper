47c3ce8dc3da5cfd9273ba478b06e74643899012
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-31 10:45:45 -0600

    soothing polka-dot

--- app.js
+++ app.js
@@ -16,13 +16,20 @@ const animals = [
 
 //#region logic
 
-function feedJeffrey() {
+function sootheJeffrey() {
   // ✅ get jeffrey
   const jeffrey = animals[0]
   // make mood go up by 1
   jeffrey.mood += 1
   console.log('this is jeffrey', jeffrey)
   drawJeffreyStats()
+}
+
+function soothePolkaDot() {
+  const polkaDot = animals[1]
+  polkaDot.mood += 1
+  console.log('polka dot', polkaDot);
+  drawPolkaDotStats()
 }
 
 
@@ -32,14 +39,21 @@ function feedJeffrey() {
 //#region graphics
 
 function drawJeffreyStats() {
-  // const jeffreyElem = document.getElementById('jeffrey')
-  // console.log('got the jeffrey elem', jeffreyElem);
+  const jeffreyElem = document.getElementById('jeffrey')
+  console.log('got the jeffrey elem', jeffreyElem);
 
-  const jeffreyParagraphElem = document.querySelector('p')
+  const jeffreyParagraphElem = jeffreyElem.querySelector('p')
   console.log('got the jeffrey paragraph elem', jeffreyParagraphElem);
 
   const jeffrey = animals[0]
   jeffreyParagraphElem.innerText = `Jeffrey | Mood ${jeffrey.mood}`
+}
+
+function drawPolkaDotStats() {
+  const polkaDotElem = document.getElementById('polka-dot')
+  const polkaDotParagraphElem = polkaDotElem.querySelector('p')
+  const polkaDot = animals[1]
+  polkaDotParagraphElem.innerText = `Polka-Dot | Mood ${polkaDot.mood}`
 }
 
 //#endregion
